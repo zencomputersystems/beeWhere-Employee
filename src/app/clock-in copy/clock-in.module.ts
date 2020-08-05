980a113e668fb5b1ba2cffdf14539c096a82a8bc
Hanani Rafei <hanani@zen.com.my>
2020-08-05 09:47:27 +0800

    Change main page to clockin-clock out page & refactor codes

--- src/app/clock-in copy/clock-in.module.ts
+++ src/app/clock-in copy/clock-in.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -12,7 +12,7 @@ import { Geofence } from '@ionic-native/geofence/ngx';
 import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, ClockInPageRoutingModule, MatSelectModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, IonicModule, ClockInPageRoutingModule, MatSelectModule],
   providers: [Geolocation, Geofence],
   declarations: [ClockInPage],
 })
