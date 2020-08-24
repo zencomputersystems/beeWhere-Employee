4307adf7a9044b214b5abb3efad4748028cc81b9
Hanani Rafei <hanani@zen.com.my>
2020-08-24 14:56:21 +0800

    add report page's function

--- src/app/report/report.page.ts
+++ src/app/report/report.page.ts
@@ -1,18 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Chart } from 'chart.js';
 
 @Component({
-  selector: 'app-report',
-  templateUrl: './report.page.html',
-  styleUrls: ['./report.page.scss'],
+  selector: "app-report",
+  templateUrl: "./report.page.html",
+  styleUrls: ["./report.page.scss"],
 })
 export class ReportPage implements OnInit {
+  @ViewChild("barChart") barChart;
+
+  bars: any;
+  colorArray: any;
 
   public curRTime = new Date().toISOString();
 
-  public data = require('../sampledata.json');
-  constructor() { }
+  public data = require("../sampledata.json");
+  public globalData = require("@services/_providers/global.json");
+
+  public reportType;
+
+  public searchForm: FormGroup;
+  constructor(private rpFormBuilder: FormBuilder) {
+    this.searchForm = this.rpFormBuilder.group({
+      type: [null, Validators.required],
+      duration: [null, Validators.required],
+      enableStatus: true
+    });
+  }
 
   ngOnInit() {
+    console.log(this.globalData);
+    console.log(this.searchForm);
+  }
+
+
+  reporte() {
+    console.log(this.reportType);
+    console.log(this.searchForm);
+  }
+  showReport() {
+    console.log("showReport");
+    console.log(this.searchForm);
+    console.log(this.searchForm.get("type").dirty);
+    console.log(this.searchForm.get("type").invalid);
+    if (this.searchForm.status === "VALID") {
+      console.log('success');
+    }
   }
 
 }
